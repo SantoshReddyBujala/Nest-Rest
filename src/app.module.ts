311eb52c3da0d88a352573201f2b774286db8323
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2025-03-12 21:07:14 -0400

    Prisma DB started

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { VendorModule } from './vendor/vendor.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, VendorModule],
+  imports: [UsersModule, VendorModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
